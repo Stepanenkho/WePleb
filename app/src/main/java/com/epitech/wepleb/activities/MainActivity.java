92f4018750eefbdc8d7c5655ef4ceb5f974a2682
Victor Solevic <victor@solevic.com>
2016-03-05 20:32:39 +0100

    Added ParseQuery example

--- app/src/main/java/com/epitech/wepleb/activities/MainActivity.java
+++ app/src/main/java/com/epitech/wepleb/activities/MainActivity.java
@@ -1,15 +1,55 @@
 package com.epitech.wepleb.activities;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.epitech.wepleb.R;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import java.util.List;
+
 public class MainActivity extends BaseActivity {
+
+    private TextView mUsernameText;
+
+    private ParseUser mUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mUsernameText = (TextView) findViewById(R.id.activity_main_username);
+
+        mUser = ParseUser.getCurrentUser();
+        populateView();
+        fetchContacts();
+    }
+
+    public void populateView() {
+        mUsernameText.setText(mUser.getUsername());
+    }
+
+    public void fetchContacts() {
+        /*
+        ParseQuery<ParseUser> parseQuery = new ParseQuery<>("_User");
+        parseQuery.whereEqualTo("username", mUser.getUsername());
+
+        parseQuery.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> objects, ParseException e) {
+                if (e != null) {
+
+                } else if (objects != null && objects.size() > 0){
+                    ParseUser user = objects.get(0);
+                    mUsernameText.setText("Found " + user.getUsername());
+                }
+            }
+        });
+        */
     }
 }
